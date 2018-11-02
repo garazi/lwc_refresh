60737dcb8a285c5227f280b451f9efbac1f0ae53
garazi <grewis@grewis-ltm4.internal.salesforce.com>
2018-11-02 11:33:29 -0700

    added progressExample

---
+++ force-app/main/default/aura/progressExample/progressExampleController.js
@@ -0,0 +1,32 @@
+({
+	handleStep: function(component, event, helper) {
+		var completedStep = component.get("v.completedStep");
+		var currentStep = event.getSource().get("v.value");
+        if (currentStep == 1) {
+            component.set("v.completedStep", '0');
+        }
+		if (completedStep < currentStep) {
+			console.log("ABORTING")
+			var tmp = component.find("progressCmp");
+			tmp.set("v.hasError", true);
+		} else {
+			var tmp = component.find("progressCmp");
+			tmp.set("v.hasError", false);
+			component.set("v.currentStep", currentStep);
+            console.log("currentStep: ", component.get("v.currentStep"))
+            if (currentStep == '4') {
+                component.set("v.completedStep", '4')
+            }
+		}
+	},
+	handleClick: function(component, event, helper) {
+		var currentStep = event.getSource().get("v.value");
+		component.set("v.completedStep", currentStep);
+		var nextStep = parseInt(currentStep, 10) + 1
+		nextStep = nextStep.toString();
+		var tmp = component.find("progressCmp");
+		tmp.set("v.hasError", false);
+		component.set("v.currentStep", nextStep);
+		console.log("currentStep: ", component.get("v.currentStep"))
+	}
+})
--- force-app/main/default/aura/supaFast/supaFastController.js
+++ force-app/main/default/aura/supaFast/supaFastController.js
@@ -1,14 +1,27 @@
 ({
+	handleStep: function(component, event, helper) {
+		var completedStep = component.get("v.completedStep");
+		var currentStep = event.getSource().get("v.value");
 
-	handleFocus: function(component, event, helper) {
-		alert("focused!");
-		console.log('Helloooooooooo');
-    },
-    handleStepBlur : function (cmp, evt, helper) {
-        var stepIndex = evt.getParam('index');
-        alert("f: ", stepIndex)
-    },
-    submit : function(component, event, helper) {
-		alert('submit triggered');
+		if (completedStep < currentStep) {
+      console.log("ABORTING")
+      var tmp = component.find("progressCmp");
+      tmp.set("v.hasError", true);
+		} else {
+      var tmp = component.find("progressCmp");
+      tmp.set("v.hasError", false);
+			component.set("v.currentStep", currentStep);
+			console.log("NOW: ", component.get("v.currentStep"))
+		}
+	},
+	handleClick: function(component, event, helper) {
+		var currentStep = event.getSource().get("v.value");
+    component.set("v.completedStep", currentStep);
+    var nextStep = parseInt(currentStep, 10) + 1
+    nextStep = nextStep.toString();
+    var tmp = component.find("progressCmp");
+    tmp.set("v.hasError", false);
+    component.set("v.currentStep", nextStep);
+    console.log("new value: ", component.get("v.currentStep"))
 	}
 })
