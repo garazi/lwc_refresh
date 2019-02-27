61107889f4d38bd2e93d68d83caf7ab160ae7fee
garazi <grewis@grewis-ltm4.internal.salesforce.com>
2019-02-27 13:20:56 -0800

    biz-explorer updates

--- force-app/main/default/lwc/businessExplorer/businessExplorer.js
+++ force-app/main/default/lwc/businessExplorer/businessExplorer.js
@@ -8,6 +8,9 @@ const fields = [
     'Property__c.City__c',
     'Property__c.State__c',
     'Property__c.Zip__c',
+    'Property__c.Location__Longitude__s',
+    'Property__c.Location__Latitude__s',
+    'Property__c.Title__c'
 ]
 
 export default class BusinessExplorer extends LightningElement {
@@ -20,6 +23,7 @@ export default class BusinessExplorer extends LightningElement {
     @track bizArray;
     @track rating;
     @track mapMarkers = [];
+    @api flexipageRegionWidth;
 
 
     @wire(getRecord, { recordId: '$recordId', fields })
@@ -27,6 +31,17 @@ export default class BusinessExplorer extends LightningElement {
         if (value.data) {
             this.property = value.data;
             this.address = this.property.fields.Address__c.value + ',' + this.property.fields.City__c.value + ',' + this.property.fields.State__c;
+            console.log("LOC: ", this.property.fields)
+            this.mapMarkers = [{
+                location: {
+                    Street: this.property.fields.Address__c.value,
+                    City: this.property.fields.City__c.value,
+                    State: this.property.fields.State__c.value
+                },
+                title: this.property.fields.Title__c.value,
+                description: this.address
+            }];
+            console.log("MARKERS: ", this.mapMarkers)
         } else if (value.error) {
             console.log("OOOPS: ", value.error)
         }
@@ -41,7 +56,7 @@ export default class BusinessExplorer extends LightningElement {
                 element.rating = element.rating + " stars";
             });
             console.log("DATA: ", JSON.parse(this.searchResults));
-            this.createMapMarkers(this.bizArray);
+            // this.createMapMarkers(this.bizArray);
         } else if (value.error) {
             console.log("ERROR: ", value.error);
         }
@@ -53,18 +68,18 @@ export default class BusinessExplorer extends LightningElement {
 
     createMapMarkers(bizArray) {
         let tempArray = [];
-        bizArray.forEach(element => {
-            let singleton = {
-				location: {
-					Longitude: element.location.longitude,
-					Latitude: element.location.latitude
-				},
-				title: element.name,
-				description: element.address + ', ' + element.city
-			}
-			tempArray.push(singleton);
-        });
-        this.mapMarkers = tempArray;
+        // bizArray.forEach(element => {
+        //     let singleton = {
+        // 		location: {
+        // 			Longitude: element.location.longitude,
+        // 			Latitude: element.location.latitude
+        // 		},
+        // 		title: element.name,
+        // 		description: element.address + ', ' + element.city
+        // 	}
+        // 	tempArray.push(singleton);
+        // });
+        // this.mapMarkers = tempArray;
         // console.log("MAP: ", this.mapMarkers)
     }
 }
---
+++ force-app/main/default/lwc/businessExplorerItem/businessExplorerItem.js
@@ -0,0 +1,5 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class BusinessExplorerItem extends LightningElement {
+    @api item;
+}
